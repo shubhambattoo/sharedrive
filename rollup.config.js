a6c68ba7174e2d62b67d8f1cab73c6e38cf1e91f
shubhambattoo <shubham.battoo@hotmail.com>
2021-10-31 21:12:39 +0530

    updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default (async () => ({
-    input: './public/js/index.js',
-    output: {
-        file: './public/js/bundle.js',
-        format: 'cjs'
-    },
-    plugins: [resolve(), commonjs(), isProduction && (await import('rollup-plugin-terser')).terser()],
+  input: './public/js/index.js',
+  output: {
+    file: './public/js/bundle.js',
+    format: 'cjs'
+  },
+  plugins: [resolve(), commonjs(), isProduction && (await import('rollup-plugin-terser')).terser()],
 }))();

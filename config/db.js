9ba2125c08cd2d46fb1029187cac273245607001
shubhambattoo <shubham.battoo@hotmail.com>
2021-10-31 21:43:17 +0530

    added cors config

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
 function connectDb() {
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(','),
+};
 app.use(express.json({ extended: false }));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors(corsOptions));
 
 const connectDb = require('./config/db');
 

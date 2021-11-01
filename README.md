ea8536c15a420f6eda4d98c07f836acedd9ff597
shubhambattoo <shubham.battoo@hotmail.com>
2021-11-01 13:16:00 +0530

    updated readme and handled client side errors

---
+++ README.md
@@ -0,0 +1,28 @@
+# ShareDrive 📁
+
+> ShareDrive is a file sharing service that allows users to share file with other users. You can easily share file with your friends and colleagues, the link expires after a certain time.
+
+## [Demo](https://sharedrive-123.herokuapp.com/)
+
+![](./github/homepage.png)
+
+## Features
+
+- [✔️] File sharing
+- [✔️] File sharing link expiration
+
+## Getting Started
+
+Clone the repository, and run the following command inside the project directory to install all the dependencies:
+
+```
+npm install
+```
+
+Create an environment file to run the application, as per the `env.example`.
+
+To start the application run the following command:
+
+```
+npm start
+```
--- public/js/index.js
+++ public/js/index.js
@@ -20,99 +20,112 @@ const copyBtn = document.getElementById('copy-btn');
 
 let selectedFile = null;
 
-dropArea.addEventListener('dragenter', dragenter, false);
-dropArea.addEventListener('dragover', dragOverHandler, false);
-dropArea.addEventListener(
-  'drop',
-  (e) => {
-    const file = dropHandler(e);
-    selectedFile = getSelectedFile(file);
-    const html = `
-    <div> ${selectedFile.name} </div>
-    <div> ${selectedFile.size} </div>    
-  `;
-    selectorArea.style.display = 'none';
-    selectorArea.setAttribute('aria-hidden', 'true');
-    selectedArea.style.display = 'block';
-    selectedArea.setAttribute('aria-hidden', 'false');
-    fileBox.innerHTML = html;
-  },
-  false
-);
-
-fileInput.addEventListener(
-  'change',
-  (e) => {
-    const file = e.target.files[0];
-    selectedFile = getSelectedFile(file);
-    const html = `
+if (dropArea) {
+  dropArea.addEventListener('dragenter', dragenter, false);
+  dropArea.addEventListener('dragover', dragOverHandler, false);
+  dropArea.addEventListener(
+    'drop',
+    (e) => {
+      const file = dropHandler(e);
+      selectedFile = getSelectedFile(file);
+      const html = `
+      <div> ${selectedFile.name} </div>
+      <div> ${selectedFile.size} </div>    
+      `;
+      selectorArea.style.display = 'none';
+      selectorArea.setAttribute('aria-hidden', 'true');
+      selectedArea.style.display = 'block';
+      selectedArea.setAttribute('aria-hidden', 'false');
+      fileBox.innerHTML = html;
+    },
+    false
+  );
+}
+if (fileInput) {
+  fileInput.addEventListener(
+    'change',
+    (e) => {
+      const file = e.target.files[0];
+      selectedFile = getSelectedFile(file);
+      const html = `
       <div> ${selectedFile.name} </div>
       <div> ${selectedFile.size} </div>    
     `;
-    selectorArea.style.display = 'none';
-    selectorArea.setAttribute('aria-hidden', 'true');
-    selectedArea.style.display = 'block';
-    selectedArea.setAttribute('aria-hidden', 'false');
-    fileBox.innerHTML = html;
-  },
-  false
-);
+      selectorArea.style.display = 'none';
+      selectorArea.setAttribute('aria-hidden', 'true');
+      selectedArea.style.display = 'block';
+      selectedArea.setAttribute('aria-hidden', 'false');
+      fileBox.innerHTML = html;
+    },
+    false
+  );
+}
 
-removeBtn.addEventListener('click', () => {
-  selectedArea.style.display = 'none';
-  selectedArea.setAttribute('aria-hidden', 'true');
-  selectorArea.style.display = 'block';
-  selectorArea.setAttribute('aria-hidden', 'false');
-  fileBox.innerHTML = '';
-  selectedFile = null;
-}, false);
+if (removeBtn) {
+  removeBtn.addEventListener('click', () => {
+    selectedArea.style.display = 'none';
+    selectedArea.setAttribute('aria-hidden', 'true');
+    selectorArea.style.display = 'block';
+    selectorArea.setAttribute('aria-hidden', 'false');
+    fileBox.innerHTML = '';
+    selectedFile = null;
+  }, false);
+}
 
-uploadBtn.addEventListener('click', async () => {
-  const html = `
+if (uploadBtn) {
+
+
+  uploadBtn.addEventListener('click', async () => {
+    const html = `
     <div> ${selectedFile.name} </div>
     <div> ${selectedFile.size} </div>    
   `;
-  loadingArea.style.display = 'block';
-  loadingArea.setAttribute('aria-hidden', 'false');
-  fileBox1.innerHTML = html;
-  selectedArea.style.display = 'none';
-  selectedArea.setAttribute('aria-hidden', 'true');
+    loadingArea.style.display = 'block';
+    loadingArea.setAttribute('aria-hidden', 'false');
+    fileBox1.innerHTML = html;
+    selectedArea.style.display = 'none';
+    selectedArea.setAttribute('aria-hidden', 'true');
 
-  try {
-    const data = await saveFile(selectedFile.blob, selectedFile.name);
-    document.getElementById('link').value = `${data.file}`;
-    fileBox2.innerHTML = `${selectedFile.name}`;
-    mainArea.style.display = 'none';
-    mainArea.setAttribute('aria-hidden', 'true');
-    fileBox1.innerHTML = '';
-    uploadedArea.style.display = 'block';
-    uploadedArea.setAttribute('aria-hidden', 'false');
-  } catch (error) {
-    alert('Could not save the file.');
+    try {
+      const data = await saveFile(selectedFile.blob, selectedFile.name);
+      document.getElementById('link').value = `${data.file}`;
+      fileBox2.innerHTML = `${selectedFile.name}`;
+      mainArea.style.display = 'none';
+      mainArea.setAttribute('aria-hidden', 'true');
+      fileBox1.innerHTML = '';
+      uploadedArea.style.display = 'block';
+      uploadedArea.setAttribute('aria-hidden', 'false');
+    } catch (error) {
+      alert('Could not save the file.');
+      selectorArea.style.display = 'block';
+      selectorArea.setAttribute('aria-hidden', 'false');
+      selectedFile = null;
+      mainArea.style.display = 'grid';
+      mainArea.setAttribute('aria-hidden', 'false');
+    } finally {
+      loadingArea.style.display = 'none';
+      loadingArea.setAttribute('aria-hidden', 'true');
+    }
+  });
+}
+
+if (okBtn) {
+  okBtn.addEventListener('click', () => {
+    uploadedArea.style.display = 'none';
+    uploadedArea.setAttribute('aria-hidden', 'true');
     selectorArea.style.display = 'block';
     selectorArea.setAttribute('aria-hidden', 'false');
+    fileBox.innerHTML = '';
     selectedFile = null;
     mainArea.style.display = 'grid';
     mainArea.setAttribute('aria-hidden', 'false');
-  } finally {
-    loadingArea.style.display = 'none';
-    loadingArea.setAttribute('aria-hidden', 'true');
-  }
-});
+  }, false);
+}
 
-okBtn.addEventListener('click', () => {
-  uploadedArea.style.display = 'none';
-  uploadedArea.setAttribute('aria-hidden', 'true');
-  selectorArea.style.display = 'block';
-  selectorArea.setAttribute('aria-hidden', 'false');
-  fileBox.innerHTML = '';
-  selectedFile = null;
-  mainArea.style.display = 'grid';
-  mainArea.setAttribute('aria-hidden', 'false');
-}, false);
-
-copyBtn.addEventListener('click', function() {
-  const copyText = document.getElementById('link');
-  copy(copyText.value);
-  this.innerHTML = 'Copied!';
-}, false);
+if (copyBtn) {
+  copyBtn.addEventListener('click', function () {
+    const copyText = document.getElementById('link');
+    copy(copyText.value);
+    this.innerHTML = 'Copied!';
+  }, false);
+}
